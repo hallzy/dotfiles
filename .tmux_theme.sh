33dbdcee1935e99f32c0536a4ee36d478c77d5fc
Steven Hall <Hallzy.18@gmail.com>
2016-05-16 23:22:27 -0700

    Changed colour and format of status line elements

--- .tmux_theme.sh
+++ .tmux_theme.sh
@@ -7,6 +7,13 @@ tm_color_feature=colour10
 tm_color_music=colour10
 tm_active_border_color=colour10
 
+tm_color_time=colour9
+tm_color_day=colour190
+tm_color_date=colour150
+
+tm_color_window_names=colour172
+tm_color_status_bg=colour235
+
 # separators
 tm_separator_left_bold="◀"
 tm_separator_left_thin="❮"
@@ -19,9 +26,8 @@ set -g status-interval 5
 
 
 # default statusbar colors
-# set-option -g status-bg colour0
 set-option -g status-fg $tm_color_active
-set-option -g status-bg default
+set-option -g status-bg $tm_color_status_bg
 set-option -g status-attr default
 
 # default window title colors
@@ -30,7 +36,7 @@ set-window-option -g window-status-bg default
 set -g window-status-format "#I #W"
 
 # active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
+set-window-option -g window-status-current-fg $tm_color_window_names
 set-window-option -g window-status-current-bg default
 set-window-option -g  window-status-current-format "#[bold]#I #W"
 
@@ -49,10 +55,13 @@ set-option -g display-panes-colour $tm_color_inactive
 # clock
 set-window-option -g clock-mode-colour $tm_color_active
 
-tm_date="#[fg=$tm_color_inactive] %R %d %b"
+# example: 10:34 PM - Monday - May 16
+tm_time="#[fg=$tm_color_time,bold]%l:%M %p"
+tm_day="#[fg=$tm_color_day,bold]%A"
+tm_date="#[fg=$tm_color_date,bold]%b %d"
 tm_host="#[fg=$tm_color_feature,bold]#h"
-tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
+tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon#S$tm_icon"
 
-set -g status-left $tm_session_name' '
-set -g status-right $tm_itunes' '$tm_date' '$tm_host
+set -g status-left $tm_session_name'  '
+set -g status-right $tm_itunes' '$tm_time' '$tm_day' '$tm_date' '$tm_host
 
