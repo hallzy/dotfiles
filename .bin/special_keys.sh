3cd05cecc79da9e9653b773833c325bcdc914d03
Steven Hall <Hallzy.18@gmail.com>
2020-05-04 16:35:35 -0700

    Volume down/up and brightness down/up button in DWM

---
+++ .bin/special_keys.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+debug() {
+    notify-send "Special Keys Debug" "$1"
+}
+
+voltoggle() {
+    pactl set-sink-mute 0 toggle
+    # debug "volume toggle"
+}
+
+voldown() {
+    pactl set-sink-volume 0 '-5%'
+    # debug "volume down"
+}
+
+volup() {
+    pactl set-sink-volume 0 '+5%'
+    # debug "volume up"
+}
+
+changeBrightness() {
+    local brightness
+    local max
+    local new
+    local sign
+
+    sign="$1"
+
+    brightness="$(cat /sys/class/backlight/intel_backlight/brightness)"
+    max="$(cat /sys/class/backlight/intel_backlight/max_brightness)"
+
+    new=$(python -c "print $brightness $sign 50")
+
+    # Convert to int
+    if [ "$new" -lt 0 ]; then
+        new=0
+    elif [ "$new" -gt "$max" ]; then
+        new="$max"
+    fi
+
+    echo "$new" | tee /sys/class/backlight/intel_backlight/brightness
+}
+
+brightup() {
+    changeBrightness '+'
+}
+
+brightdown() {
+    changeBrightness '-'
+}
+
+
+## Argument to script call is one of the above functions
+$1
