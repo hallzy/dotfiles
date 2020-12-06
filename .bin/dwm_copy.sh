29c781dedac96d13d96064b8e6b3dcbb6c71c1f1
Steven Hall <Hallzy.18@gmail.com>
2020-12-05 17:13:49 -0800

    Script for copying and opening text from st terminal using externalpipe

---
+++ .bin/dwm_copy.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# This script is to be used in conjunction with the external pipe feature of st
+
+urlCommon() {
+    sed 's/.*│//g' /dev/stdin | \
+        tr -d '\n' | \
+        grep -aEo '(((http|https)://|www\\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./&%?$#=_-]*)|((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)' | \
+        uniq | \
+        tac | \
+        sed 's/^www./http:\/\/www\./g' | \
+        dmenu -i -l 10 -p "$1"
+}
+
+copyURL() {
+    urlCommon "Copy Which URL?" | tr -d '\n' | xclip -selection clipboard
+}
+
+openURL() {
+    urlCommon 'Follow which url?' | xargs -r xdg-open
+}
+
+copydebug() {
+    debug
+}
+
+opendebug() {
+    debug
+}
+
+debug() {
+    cat /dev/stdin > /tmp/dwm-copy.tmp
+    err -w "stdin written to /tmp/dwm-copy.tmp"
+}
+
+if [ -z "$1" ]; then
+    err -e "You need to specify if this is a 'copy' or 'open' command."
+    exit 1
+fi
+
+# copyOptions=$(cat <<EOF
+# URL
+# EOF
+# )
+
+# openOptions=$(cat <<EOF
+# URL
+# EOF
+# )
+
+if [ "$1" = "copy" ]; then
+    copyURL
+    exit 0
+    # func="copy$(echo "$copyOptions" | dmenu -i -l 10 -p "What to Copy?")"
+elif [ "$1" = "open" ]; then
+    openURL
+    exit 0
+    # func="open$(echo "$openOptions" | dmenu -i -l 10 -p "What to Open?")"
+else
+    err -e "'$1' is not valid. Must be 'copy' or 'open'"
+    exit 1
+fi
+
+# if [ "$func" = "copy" ] || [ "$func" = "open" ]; then
+#     exit 1
+# fi
+
+# $func
