c53677e6b21c9dd4d2d501995776f010d7835cd6
Steven Hall <Hallzy.18@gmail.com>
2021-05-23 10:23:24 -0700

    Script to fix libxft reference

---
+++ .bin/fix-emoji-library.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ubuntu doesn't have the updated lib xft library that allows emojis, so without
+# the new version I manually installed, it will crash st, dmenu, and dwm if an
+# emoji is shown.
+
+# Every once in a while the library is overwritten by Ubuntu (I think it happens
+# whenever I install or update something and apt replaces the library) so I have
+# to manually overwrite it every once in a while.
+
+MANUALLY_INSTALLED_VERSION="/usr/lib/libXft.so.2"
+TARGET_LOCATION="/usr/lib/x86_64-linux-gnu/libXft.so.2"
+
+if [ ! -f "$MANUALLY_INSTALLED_VERSION" ]; then
+    echo "Manually installed version file does not exist [${MANUALLY_INSTALLED_VERSION}]"
+    exit 1
+fi
+
+if [ ! -f "$TARGET_LOCATION" ]; then
+    echo "Target file does not exist [${TARGET_LOCATION}]"
+    exit 1
+fi
+
+sudo ln -sf "$MANUALLY_INSTALLED_VERSION" "$TARGET_LOCATION"
