8b5276a95eba93b8ea989a0ec369bcf024f9c7c7
Steven Hall <Hallzy.18@gmail.com>
2021-11-01 17:49:53 -0700

    If loopfile is provided as an argument, then ignore my custom repeat rule and just do what the loopfile argument says

--- .config/mpv/scripts/mpv.lua
+++ .config/mpv/scripts/mpv.lua
@@ -1,36 +1,41 @@
--- Create a timer that will trigger every 6 seconds.
-timer = mp.add_periodic_timer(6, function()
-    -- The timer will end looping for this file and then stop and reset itself
-    mp.set_property("loop-file", "no");
+loopFile = mp.get_property("loop-file");
+
+-- If LoopFile is not on, then loop the file until it exceeds 6 seconds
+if loopFile == "no" then
+    -- Create a timer that will trigger every 6 seconds.
+    timer = mp.add_periodic_timer(6, function()
+        -- The timer will end looping for this file and then stop and reset itself
+        mp.set_property("loop-file", "no");
+        timer:kill();
+    end);
+
+    -- Start with the timer stopped and reset
     timer:kill();
-end);
 
--- Start with the timer stopped and reset
-timer:kill();
-
--- When a file is loaded turn looping back on and resume the timer
-mp.register_event("file-loaded", function()
-    filePath = mp.get_property("path");
-    mp.commandv("run", "/bin/sh", "-c", "echo 'FILE PATH: " .. filePath .. "'");
-    mp.set_property("loop-file", "yes");
-    timer:resume();
-end);
-
--- When a file is ended kill the timer so that it resets
-mp.register_event("end-file", function()
-    timer:kill();
-end);
-
--- Watch for a pause
-mp.observe_property("pause", "bool", function(name, value)
-    -- If playback is paused, then pause the timer
-    if value == true then
-        timer:stop();
-    -- If now we are unpaused, then unpause the timer
-    else
+    -- When a file is loaded turn looping back on and resume the timer
+    mp.register_event("file-loaded", function()
+        filePath = mp.get_property("path");
+        mp.commandv("run", "/bin/sh", "-c", "echo 'FILE PATH: " .. filePath .. "'");
+        mp.set_property("loop-file", "yes");
         timer:resume();
-    end
-end);
+    end);
+
+    -- When a file is ended kill the timer so that it resets
+    mp.register_event("end-file", function()
+        timer:kill();
+    end);
+
+    -- Watch for a pause
+    mp.observe_property("pause", "bool", function(name, value)
+        -- If playback is paused, then pause the timer
+        if value == true then
+            timer:stop();
+        -- If now we are unpaused, then unpause the timer
+        else
+            timer:resume();
+        end
+    end);
+end
 
 -- Ctrl+x will open the current file in sxiv
 mp.add_key_binding("ctrl+x", function()
