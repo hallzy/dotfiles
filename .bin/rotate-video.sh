fa18ea3ff98ba86246499b105786f6d9d0cd276c
Steven Hall <Hallzy.18@gmail.com>
2025-03-22 17:12:04 -0700

    Update rotate videos script

--- .bin/rotate-video.sh
+++ .bin/rotate-video.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
+usage() {
+    echo ""
+    echo "rotate-video.sh <cw|ccw> <input file> <output file>"
+}
+
 dir=$1
 inputFile=$2
 outputFile=$3
-
-if ! [ -f "$inputFile" ]; then
-    echo "Input file [$inputFile] does not exist";
-    exit 1
-fi
-
-if [ -f "$outputFile" ]; then
-    echo "Output file [$outputFile] already exists";
-    exit 1
-fi
 
 if [ "$dir" = 'cw' ]; then
     dir=1
 elif [ "$dir" = "ccw" ]; then
     dir=2
+else
+    echo "Unknown Direction [$dir]";
+    usage
+    exit 1
+fi
+
+if ! [ -f "$inputFile" ]; then
+    echo "Input file [$inputFile] does not exist";
+    usage
+    exit 1
+fi
+
+if [ -f "$outputFile" ]; then
+    echo "Output file [$outputFile] already exists";
+    usage
+    exit 1
 fi
 
 ffmpeg -i "$inputFile" -vf "transpose=${dir}" "$outputFile"
