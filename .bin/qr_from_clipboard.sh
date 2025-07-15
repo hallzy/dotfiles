99548be11074ccab3efe35af67870e53e464225b
Steven Hall <Hallzy.18@gmail.com>
2025-07-15 09:25:10 -0700

    Only use the clipboard, not the selection

--- .bin/qr_from_clipboard.sh
+++ .bin/qr_from_clipboard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Grab clipboard contents
-CLIP=$(xclip -o)
+CLIP=$(xclip -selection clipboard -o)
 
 # Run in new st terminal and keep it open
 st -e bash -c "echo \"$CLIP\"; echo ''; qrencode -t ANSIUTF8 \"$CLIP\"; echo ''; echo 'Press Enter to close...'; read"
